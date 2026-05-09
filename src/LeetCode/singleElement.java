57673491fd1eacd812739b566e6745f7e9e3d4f1
Joydev Barik <barikjoydev263@gmail.com>
2026-05-09 17:41:07 +0530

    singleELement by Hash map approach

---
+++ src/LeetCode/singleElement.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class singleElement {
+
+    public static int singleElement(int[] nums) {
+
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int ans = singleElement(nums);
+
+        System.out.println("Single element is: " + ans);
+
+        sc.close();
+    }
+}
