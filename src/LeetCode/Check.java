ba047b6db0d339658ef51f72bb47a497b25ec611
Joydev Barik <barikjoydev263@gmail.com>
2026-05-16 19:35:43 +0530

    Check Prime

---
+++ src/LeetCode/Check.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Check {
+
+    public static boolean isPrime(int n) {
+
+        if (n <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i < n; i++) {
+
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(isPrime(n));
+    }
+}
