9bd1902974db0d3088839a91878eed0e7acdf7fa
Joydev Barik <barikjoydev263@gmail.com>
2026-05-21 11:11:06 +0530

    ReorganizeStrings by MAX Heap Approach

---
+++ src/LeetCode/ReorganizeString.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class ReorganizeString {
+
+    public static String reorganizeString(String s) {
+
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray()) {
+
+            freq[c - 'a']++;
+        }
+
+        int n = s.length();
+
+        PriorityQueue<int[]> heap =
+                new PriorityQueue<>((a, b) -> b[0] - a[0]);
+
+        for (int i = 0; i < 26; i++) {
+
+            if (freq[i] > 0) {
+
+                if (freq[i] > (n + 1) / 2) {
+
+                    return "";
+                }
+
+                heap.offer(new int[]{freq[i], i});
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        int[] prev = null;
+
+        while (!heap.isEmpty()) {
+
+            int[] curr = heap.poll();
+
+            if (prev != null && prev[0] > 0) {
+
+                heap.offer(prev);
+            }
+
+            result.append((char)(curr[1] + 'a'));
+
+            curr[0]--;
+
+            prev = curr;
+        }
+
+        return result.length() == n
+                ? result.toString()
+                : "";
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        System.out.println(reorganizeString(s));
+
+        sc.close();
+    }
+}
