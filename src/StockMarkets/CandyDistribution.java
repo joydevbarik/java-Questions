7060b4d1c45ddebea290a560e8d40fa2d2a9cb62
Joydev Barik <barikjoydev263@gmail.com>
2026-05-28 11:50:44 +0530

    Candy distribution

---
+++ src/StockMarkets/CandyDistribution.java
@@ -0,0 +1,43 @@
+public class CandyDistribution {
+
+    public static int candy(int[] ratings) {
+
+        int n = ratings.length;
+
+        int[] candies = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            candies[i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            if (ratings[i] > ratings[i + 1]) {
+
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int total = 0;
+
+        for (int x : candies) {
+            total += x;
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+
+        int[] ratings = {1,0,2};
+
+        System.out.println(candy(ratings));
+    }
+}
