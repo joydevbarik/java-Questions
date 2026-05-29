3c5a7d9dca2a90a9b5ccc9da3eca40562ee32811
Joydev Barik <barikjoydev263@gmail.com>
2026-05-29 10:44:43 +0530

    Count digits loop

---
+++ src/Loops/CountDigitsLoop.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CountDigitsLoop {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        while (n > 0) {
+
+            count++;
+
+            n /= 10;
+        }
+
+        System.out.println("Digits = " + count);
+
+        sc.close();
+    }
+}
