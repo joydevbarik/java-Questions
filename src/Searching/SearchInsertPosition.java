55c254993d727f6ec80101e88d4c793f39ad98bc
Joydev Barik <barikjoydev263@gmail.com>
2026-06-02 09:40:47 +0530

    Search Insert Position

---
+++ src/Searching/SearchInsertPosition.java
@@ -0,0 +1,34 @@
+public class SearchInsertPosition {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 5, 6};
+
+        int target = 2;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+
+                System.out.println(mid);
+                return;
+            }
+
+            if (arr[mid] < target) {
+
+                left = mid + 1;
+
+            } else {
+
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(left);
+    }
+}
