46dbb891633a99f18ecd043361d924a7ef0a7ccb
Joydev Barik <barikjoydev263@gmail.com>
2026-06-09 22:49:15 +0530

    Constructor problem

---
+++ src/OOPS/Constructor.java
@@ -0,0 +1,12 @@
+public class Constructor {
+
+    static class Student {
+        Student() {
+            System.out.println("Constructor Called");
+        }
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student();
+    }
+}
