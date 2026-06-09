618d1b9874fea0f945ef4b32042e874f859a9960
Joydev Barik <barikjoydev263@gmail.com>
2026-06-09 22:51:39 +0530

    Method Overloading

---
+++ src/OOPS/Overloading.java
@@ -0,0 +1,21 @@
+public class Overloading {
+
+    static class Calculator {
+
+        int add(int a, int b) {
+            return a + b;
+        }
+
+        int add(int a, int b, int c) {
+            return a + b + c;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+
+        System.out.println(c.add(5, 3));
+        System.out.println(c.add(5, 3, 2));
+    }
+}
