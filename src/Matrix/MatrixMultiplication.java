e4c95a3202db91ff99ef1db8ef177da092610dfe
Joydev Barik <barikjoydev263@gmail.com>
2026-06-19 20:30:34 +0530

    Matrix multiplication

---
+++ src/Matrix/MatrixMultiplication.java
@@ -0,0 +1,31 @@
+public class MatrixMultiplication {
+
+    public static void main(String[] args) {
+
+        int[][] A = {
+                {1,2},
+                {3,4}
+        };
+
+        int[][] B = {
+                {5,6},
+                {7,8}
+        };
+
+        int[][] C = new int[2][2];
+
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                for(int k=0;k<2;k++){
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        for(int[] row : C){
+            for(int num : row)
+                System.out.print(num+" ");
+            System.out.println();
+        }
+    }
+}
