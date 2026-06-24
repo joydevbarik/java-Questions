42b1873a15181be4e8f2c789c68d8ae58572f97d
Joydev Barik <barikjoydev263@gmail.com>
2026-06-24 21:37:55 +0530

    Maximum Subarray Profit

---
+++ src/StockMarkets/KadaneStockProblem.java
@@ -0,0 +1,37 @@
+public class KadaneStockProblem {
+
+
+    public static int maxSum(int arr[]){
+
+        int current=arr[0];
+        int max=arr[0];
+
+
+        for(int i=1;i<arr.length;i++){
+
+            current=Math.max(
+                    arr[i],
+                    current+arr[i]
+            );
+
+
+            max=Math.max(
+                    max,
+                    current
+            );
+        }
+
+
+        return max;
+    }
+
+
+    public static void main(String[] args){
+
+        int arr[]={-2,1,-3,4,-1,2,1};
+
+        System.out.println(
+                maxSum(arr)
+        );
+    }
+}
