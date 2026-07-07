28b68b6872b18617fdd086e373376ff9b7a9f2c1
Joydev Barik <barikjoydev263@gmail.com>
2026-07-07 05:36:10 +0530

    Max consecutive ones problem

---
+++ src/Pointers/MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+public class MaxConsecutiveOnes {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,1,0,1,1,1};
+
+        int count = 0;
+        int max = 0;
+
+        for (int x : arr) {
+
+            if (x == 1) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
