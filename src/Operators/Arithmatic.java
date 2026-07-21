340b085651dcfd9d754f5849b52e75f86abfd1b5
Joydev Barik <barikjoydev263@gmail.com>
2026-07-21 20:02:55 +0530

    Arithmatic problems

---
+++ src/Operators/Arithmatic.java
@@ -0,0 +1,15 @@
+public class Arithmatic {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 100;
+
+        System.out.println("The addition value is:-" );
+        System.out.println(a+b);
+        System.out.println("The subtraction value is:- " );
+        System.out.println(a-b);
+        System.out.println("The multiplication value is:- ");
+        System.out.println(a*b);
+        System.out.println("The division value is:- " );
+        System.out.println(a/b);
+       }
+}
